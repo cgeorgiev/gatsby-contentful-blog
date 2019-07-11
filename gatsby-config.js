751e91668261e21201e520e08e5080ec962779c0
Tsvetomir Georgiev <ceco.georgiev@gmail.com>
2019-07-11 17:03:36 +0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Awesome Blog`,
+    description: `An awesome blog displaying my awesome posts.`,
+    author: `Ceco`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `xd8z8keid4x6`,
+        accessToken: `DZNdIyr821RTbIdaXNxFZX6vF7vRNJajb0Fg5ExMDi0`
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
