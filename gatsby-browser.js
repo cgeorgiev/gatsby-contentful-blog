7fc8c5cc8609b9a8bc5235f0bca92487391d13e2
Tsvetomir Georgiev <ceco.georgiev@gmail.com>
2019-07-12 20:09:42 +0300

    10

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,3 +9,14 @@
 // export const onClientEntry = () => {
 //   window.onload = () => {}
 // }
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
